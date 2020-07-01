1049d226513a01e20284fc32df06bfc21fa8c628
LenaPuh <LenaPuh95@gmail.com>
2020-07-01 17:13:40 +0300

    Ok

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,22 @@
 import random
+
+
 def primary():
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-  last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    last = len(quotes) - 1
+    rnd = random.randint(0, last)
+    print(quotes[rnd].rstrip('\n'))
+    rnd = random.randint(0, last)
+    print(quotes[rnd].rstrip('\n'))
+    rnd = random.randint(0, last)
+    print(quotes[rnd].rstrip('\n'))
 
-if __name__== "__main__":
-  primary()
+
+with open('quotes.txt', 'a') as ouf:
+    ouf.write(input('Insert your quote:') + '\n')
+
+if __name__ == "__main__":
+    primary()
